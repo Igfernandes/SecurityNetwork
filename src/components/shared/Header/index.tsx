63da98298db0cfa8900febee65eb5288440ff148
IGFernandes <igor.fernandes@sidea.com.br>
2023-06-25 19:17:12 -0300

    feat: costume header, btn-default and others

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -5,63 +5,69 @@ import { menu } from "src/constants/menu";
 import { EmailIcon } from "src/assets/icons/Email";
 import { LocationIcon } from "src/assets/icons/Location";
 import { SocialGroups } from "./SocialGroups";
+import { Container } from "../Container";
 
 export function Header() {
   const {} = useHeader();
 
   return (
     <header>
-      <div className="header-contacts flex">
-        <div className="contacts-group flex w-[40%]">
-          <div className="contact-mail flex">
-            <EmailIcon
-              fill="#fff"
-              width={'15px'}
-              height={'15px'}
-              className="mr-[10px] flex items-center"
-            />
-            <p className="text-white text-sm">
-              E-mail: &nbsp;
-              <Link href={"mailto:companymarketbr@gmail.com"} target="_blank">
-                companymarketbr@gmail.com
-              </Link>
-            </p>
+      <Container className="bg-common-pourple px-5 py-[.75rem]">
+        <div className="header-contacts flex  ">
+          <div className="contacts-group flex w-[40%]">
+            <div className="contact-mail flex">
+              <EmailIcon
+                fill="#fff"
+                width={"15px"}
+                height={"15px"}
+                className="mr-[10px] flex items-center"
+              />
+              <p className="text-color-white text-sm">
+                E-mail: &nbsp;
+                <Link href={"mailto:companymarketbr@gmail.com"} target="_blank">
+                  companymarketbr@gmail.com
+                </Link>
+              </p>
+            </div>
+            <div className="location flex ml-5">
+              <LocationIcon
+                fill="#fff"
+                width={"15px"}
+                height={"15px"}
+                className="mr-[10px] flex items-center"
+              />
+              <p className="text-color-white text-sm">
+                <Link
+                  href={
+                    "https://www.google.com/maps?client=opera-gx&hs=3B4&output=search&q=marica&entry=mc&sa=X&ved=2ahUKEwiZm4X0ucv_AhXQvJUCHbwnDXYQ0pQJegQICxAB"
+                  }
+                  target="_blank"
+                >
+                  Maricá - RJ/Brasil
+                </Link>
+              </p>
+            </div>
           </div>
-          <div className="location flex ml-5">
-            <LocationIcon
-              fill="#fff"
-              width={'15px'}
-              height={'15px'}
-              className="mr-[10px] flex items-center"
-            />
-            <p className="text-white text-sm ">
-              <Link
-                href={
-                  "https://www.google.com/maps?client=opera-gx&hs=3B4&output=search&q=marica&entry=mc&sa=X&ved=2ahUKEwiZm4X0ucv_AhXQvJUCHbwnDXYQ0pQJegQICxAB"
-                }
-                target="_blank"
-              >
-                Maricá - RJ/Brasil
-              </Link>
-            </p>
-          </div>
-        </div>
-        <div className="w-[60%]">
-          <div className="social-group w-[6rem] ml-auto">
-            <SocialGroups />
-          </div>
-        </div>
-      </div>
-      <div className="header-menu flex">
-        <div className="logo">
-          <div className="img">
-            <img src="" alt="" />
+          <div className="w-[60%]">
+            <div className="social-group w-[6rem] ml-auto">
+              <SocialGroups />
+            </div>
           </div>
         </div>
-        <div className="menu">
+      </Container>
+      <Container>
+      <div className="header-menu flex items-center">
+        <div className="logo w-[25%]">
+           <h1 className="text-color-white text-2xl font-semibold">SECURITY NETWORK</h1>
+        </div>
+        <div className="menu w-[40%]">
           <Navbar menu={menu} />
         </div>
+        <div className="menu w-[20%] ml-auto text-end">
+          <button className="btn btn-default text-sm">SOLICITE AJUDA</button>
+        </div>
       </div>
+      </Container>
     </header>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import { Header } from "src/components/shared/Header";
 import "../style/globals.css";
 import type { AppProps } from "next/app";
-import { Container } from "src/components/shared/Container";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Container>
-        <Header />
-        <Component {...pageProps} />
-      </Container>
+      <Header />
+      <Component {...pageProps} />
     </>
   );
 }
