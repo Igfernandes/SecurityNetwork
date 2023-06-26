cce4da1c2be8b6696ddd184befe24bea649c5467
jofremarques <jofremartinsmarques@gmail.com>
2023-06-26 12:26:40 -0300

    fix: remove content

--- src/components/CyberDefenses/hooks/useCyberDefenses.tsx
+++ src/components/CyberDefenses/hooks/useCyberDefenses.tsx
@@ -1,15 +1,2 @@
-import { useState } from "react";
-import Swiper from "swiper";
-
 export function useCyberDefenses() {
-    const [thumbsSwiper, setThumbsSwiper] = useState<Swiper>(new Swiper());
-    const onSwiper = (swiper: Swiper) => {
-        setThumbsSwiper(swiper);
-    }
-
-    return {
-        onSwiper,
-        setThumbsSwiper,
-        thumbsSwiper
-    }
 }
